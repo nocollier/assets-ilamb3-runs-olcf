94913f1b6ed053d9a5cf9e2ba4e84039ab0677e4
Nathan Collier <nathaniel.collier@gmail.com>
2026-06-12 17:27:01 -0400

    update

--- fetch.sh
+++ fetch.sh
@@ -1,17 +1,25 @@
 
 # We recommend that you run this on a data transfer node and most of what
 # happens here is downloading of files.
+export ILAMB_ROOT=/lustre/orion/cli137/world-shared/ilamb3-data
+export ESGF_ROOT=/lustre/orion/cli137/world-shared/ESGF-data
 
-MODELS=("BCC-ESM1" "CanESM5" "CESM2" "E3SM-1-1" "EC-Earth3-Veg" "GFDL-ESM4" "GISS-E2-1-G" "IPSL-CM6A-LR" "MIROC-ES2L" "MPI-ESM1-2-LR" "NorESM2-LM" "UKESM1-0-LL")
+# Get reference data
+ilamb fetch .venv/lib/python3.12/site-packages/ilamb3/configure/ilamb.yaml
+ilamb fetch .venv/lib/python3.12/site-packages/ilamb3/configure/iomb.yaml
 
-#mkdir -p _land
-#for model in "${MODELS[@]}"
-#do
-#  echo "Querying ESGF for $model..."
-#  ilamb esgf .venv/lib/python3.12/site-packages/ilamb3/configure/ilamb.yaml --source-id $model
-#done
-#mv *.csv _land/
+# Land model data
+MODELS=("BCC-ESM1" "CanESM5" "CESM2" "E3SM-1-1" "EC-Earth3-Veg" "GFDL-ESM4" "GISS-E2-1-G" "IPSL-CM6A-LR" "MIROC-ES2L" "MPI-ESM1-2-LR" "UKESM1-0-LL")
+mkdir -p _land
+for model in "${MODELS[@]}"
+do
+  echo "Querying ESGF for $model..."
+  ilamb esgf .venv/lib/python3.12/site-packages/ilamb3/configure/ilamb.yaml --source-id $model
+done
+mv *.csv _land/
 
+# Ocean model data
+MODELS=("ACCESS-ESM1-5" "CanESM5" "CESM2" "CMCC-ESM2" "CNRM-ESM2-1" "GFDL-ESM4" "GISS-E2-1-G" "IPSL-CM6A-LR" "MIROC-ES2L" "MPI-ESM1-2-LR" "UKESM1-0-LL")
 mkdir -p _ocean
 for model in "${MODELS[@]}"
 do
--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=ilamb3
 #SBATCH --account=cli137
-#SBATCH --time=4:00:00
+#SBATCH --time=3:00:00
 #SBATCH --nodes=4
 #SBATCH --output=%x.log
 
@@ -44,8 +44,14 @@ $SLURM_SUBMIT_DIR/.venv/bin/ilamb run \
 # Ocean run
 srun -n 16 --cpu-bind=cores --distribution=cyclic python -m mpi4py.futures \
 $SLURM_SUBMIT_DIR/.venv/bin/ilamb run \
-/ccs/home/nate/ilamb3/ilamb3/configure/iomb.yaml \
+.venv/lib/python3.12/site-packages/ilamb3/configure/iomb.yaml \
+--model-db _ocean/ACCESS-ESM1-5.csv \
+--model-db _ocean/BCC-ESM1.csv \
 --model-db _ocean/CanESM5.csv \
+--model-db _ocean/CMCC-ESM2.csv \
+--model-db _ocean/GISS-E2-1-G.csv \
+--model-db _ocean/MPI-ESM1-2-LR.csv \
+--model-db _ocean/UKESM1-0-LL.csv \
 --output-path _build/Ocean \
 --cache \
 --central-longitude -155.0 \
